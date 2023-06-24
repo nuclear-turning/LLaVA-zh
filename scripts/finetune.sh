08b32b0a5f2fdc88768c9444742fd30fc6caed26
turning <49973091+nuclear-turning@users.noreply.github.com>
2023-06-24 23:04:32 +0800

    Update finetune.sh

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -3,17 +3,17 @@ source activate
 conda activate
 torchrun --nnodes=1 --nproc_per_node=$GPU_NUM --master_port=25001 \
     llava/train/train_mem.py \
-    --model_name_or_path $GEMINI_DATA_IN1/Chinese-alpaca-13b-plus \
+    --model_name_or_path $GEMINI_DATA_IN2/Chinese-alpaca-13b-plus \
     --version 1 \
-    --data_path $GEMINI_DATA_IN2/llava_zh_instruct/llava_instruct_150k_zh.json \
-    --image_folder $GEMINI_DATA_IN2/llava_zh_instruct/images \
-    --vision_tower $GEMINI_DATA_IN1/chinese-clip-vit-large-patch14 \
+    --data_path $GEMINI_DATA_IN1/llava_instruct_zh.json \
+    --image_folder $GEMINI_DATA_IN1/images \
+    --vision_tower $GEMINI_DATA_IN3/chinese-clip-vit-large-patch14 \
     --freeze_backbone False \
-    --pretrain_mm_mlp_adapter $GEMINI_DATA_IN1/llava-zh-mm-projector.bin \
+    --pretrain_mm_mlp_adapter $GEMINI_DATA_IN1/mm_projector/llava-13b-zh-mm-projector.bin \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end True \
     --bf16 True \
-    --output_dir $GEMINI_DATA_OUT/checkpoints/llava-zh-13b-finetune \
+    --output_dir $GEMINI_DATA_OUT/llava-zh-13b-finetune \
     --num_train_epochs 3 \
     --per_device_train_batch_size $TRAIN_BATCH \
     --per_device_eval_batch_size $EVAL_BATCH \
