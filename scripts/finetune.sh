f9c92dd72660806d36efab788752d9d9d4d757e6
turning <49973091+nuclear-turning@users.noreply.github.com>
2023-06-24 22:13:44 +0800

    Update finetune.sh

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -1,19 +1,22 @@
-export CUDA_VISIBLE_DEVICES=3,5,6
-torchrun --nnodes=1 --nproc_per_node=1 --master_port=25001 \
+export PATH="/root/miniconda3/bin:$PATH"
+source activate
+conda activate
+torchrun --nnodes=1 --nproc_per_node=$GPU_NUM --master_port=25001 \
     llava/train/train_mem.py \
-    --model_name_or_path /home/gpuall/hehx/PretrainedModels/LanguageModels/ChatModels/Chinese-Vicuna-7b \
-    --data_path /home/gpuall/hehx/MLLM/data/vqa/CH-QA.json \
-    --image_folder /home/gpuall/hehx/MLLM/data/vqa/images \
-    --vision_tower /home/gpuall/hehx/PretrainedModels/MultiModalModels/chinese-clip-vit-large-patch14 \
-    --freeze_backbone True \
-    --pretrain_mm_mlp_adapter /home/gpuall/hehx/MLLM/LLaVA-zh_/checkpoints/mm_projector/llava-7b-zh-pretrain-v1.bin \
+    --model_name_or_path $GEMINI_DATA_IN1/Chinese-alpaca-13b-plus \
+    --version 1 \
+    --data_path $GEMINI_DATA_IN2/llava_zh_instruct/llava_instruct_150k_zh.json \
+    --image_folder $GEMINI_DATA_IN2/llava_zh_instruct/images \
+    --vision_tower $GEMINI_DATA_IN1/chinese-clip-vit-large-patch14 \
+    --freeze_backbone False \
+    --pretrain_mm_mlp_adapter $GEMINI_DATA_IN1/llava-zh-mm-projector.bin \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-7b-zh-finetune \
+    --output_dir $GEMINI_DATA_OUT/checkpoints/llava-zh-13b-finetune \
     --num_train_epochs 3 \
-    --per_device_train_batch_size 8 \
-    --per_device_eval_batch_size 4 \
+    --per_device_train_batch_size $TRAIN_BATCH \
+    --per_device_eval_batch_size $EVAL_BATCH \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
@@ -24,6 +27,7 @@ torchrun --nnodes=1 --nproc_per_node=1 --master_port=25001 \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
+    --tf32 True \
     --model_max_length 2048 \
     --gradient_checkpointing True \
     --lazy_preprocess True \
