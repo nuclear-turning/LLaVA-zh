c2703b70735123b2f1878e8e1154640f2f237f29
turning <49973091+nuclear-turning@users.noreply.github.com>
2023-06-26 17:15:14 +0800

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,24 +1,20 @@
-# export PATH="/root/miniconda3/bin:$PATH"
-# source activate
-# conda activate
-export CUDA_VISIBLE_DEVICES=1
-
-torchrun --nnodes=1 --nproc_per_node=1 --master_port=25001 \
+# export GPU_NUM=4 TRAIN_BATCH=2 EVAL_BATCH=2
+torchrun --nnodes=1 --nproc_per_node=$GPU_NUM --master_port=25001 \
     llava/train/train_mem.py \
-    --model_name_or_path /home/gpuall/hehx/PretrainedModels/LanguageModels/ChatModels/chinese-alpaca-plus-7b-hf \
-    --data_path /home/gpuall/hehx/MLLM/data/caption/ai_challenger/caption_chat.json \
-    --image_folder /home/gpuall/hehx/MLLM/data/caption/ai_challenger/images/ \
+    --model_name_or_path $GEMINI_DATA_IN1/Ziya-LLaMA-13B-v1.1 \
+    --data_path $GEMINI_DATA_IN3/caption/ai_challenger/caption_chat.json \
+    --image_folder $GEMINI_DATA_IN3/caption/ai_challenger/images/ \
     --version 1 \
-    --vision_tower /home/gpuall/hehx/PretrainedModels/MultiModalModels/chinese-clip-vit-large-patch14/ \
+    --vision_tower $GEMINI_DATA_IN2/chinese-clip-vit-large-patch14/ \
     --freeze_backbone True \
     --tune_mm_mlp_adapter True \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end \
     --bf16 False \
-    --output_dir checkpoints/llava-13b-zh-pretrain \
+    --output_dir $GEMINI_DATA_OUT/llava-zh-13b-pretrain \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 16 \
-    --per_device_eval_batch_size 8 \
+    --per_device_train_batch_size $TRAIN_BATCH \
+    --per_device_eval_batch_size $EVAL_BATCH \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
