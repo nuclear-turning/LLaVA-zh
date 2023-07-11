97f8979b486b58b81d3d8209d1b307a7dc3952f0
turning <49973091+nuclear-turning@users.noreply.github.com>
2023-07-12 00:26:25 +0800

    Update finetune.sh

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -3,8 +3,8 @@ torchrun --nnodes=1 --nproc_per_node=$GPU_NUM --master_port=25001 \
     llava/train/train_mem.py \
     --model_name_or_path $GEMINI_DATA_IN2/Chinese-alpaca-13b-plus \
     --version 1 \
-    --data_path $GEMINI_DATA_IN3/stage1/llava_instruct.json \
-    --image_folder $GEMINI_DATA_IN3/stage1/images/ \
+    --data_path $GEMINI_DATA_IN3/stage2/llava_instruct.json \
+    --image_folder $GEMINI_DATA_IN3/stage2/images/ \
     --vision_tower $GEMINI_DATA_IN3/chinese-clip-vit-large-patch14 \
     --freeze_backbone False \
     --pretrain_mm_mlp_adapter $GEMINI_DATA_IN3/mm_projector/llava-13b-zh-mm-projector.bin \
