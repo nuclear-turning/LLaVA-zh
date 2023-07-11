7cb338250d8d75347df59660b0d9b189d625022f
turning <49973091+nuclear-turning@users.noreply.github.com>
2023-07-12 01:03:17 +0800

    Update finetune.sh

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -7,11 +7,11 @@ torchrun --nnodes=1 --nproc_per_node=$GPU_NUM --master_port=25001 \
     --image_folder $GEMINI_DATA_IN3/stage2/images/ \
     --vision_tower $GEMINI_DATA_IN3/chinese-clip-vit-large-patch14 \
     --freeze_backbone False \
-    --pretrain_mm_mlp_adapter $GEMINI_DATA_IN3/mm_projector/llava-13b-zh-mm-projector.bin \
+    --pretrain_mm_mlp_adapter /model_output/hehx/llava_zh/mm_projector/llava-13b-zh-mm-projector.bin \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end True \
     --bf16 True \
-    --output_dir $GEMINI_DATA_OUT/llava-zh-13b-finetune \
+    --output_dir /model_output/hehx/llava_zh/llava-zh-13b-finetune \
     --num_train_epochs 3 \
     --per_device_train_batch_size $TRAIN_BATCH \
     --per_device_eval_batch_size $EVAL_BATCH \
